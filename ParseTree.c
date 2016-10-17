2158e22a4cbc9d162badbba5271031b3e18c649d
plof27 <olaf223224@gmail.com>
2016-10-17 12:58:07 -0400

    Shortened evaluate_tree
    Changed the function to avoid code duplication. I'm not testing it
    untill part 2 is written

--- ParseTree.c
+++ ParseTree.c
@@ -98,6 +98,7 @@ int insert_to_leftmost_nonterminal(TREE t, LIST l) {
     }
 }
 
+/*THIS FUNCTION IS BAD DON'T USE IT
 tree_data_type evaluate_tree(TREE t) {
     TREE left = t->left;
     TREE center = t->center;
@@ -313,6 +314,164 @@ tree_data_type evaluate_tree(TREE t) {
             }
         }
 
+    }
+
+}
+*/
+
+tree_data_type evaluate_tree(TREE t) {
+    TREE left = t->left;
+    TREE center = t->center;
+    TREE right = t->right;
+
+    //some of the children may be non terminals. This must be fixed.
+    //first, recurse down on each non-terminal child
+    if (left->terminal == 0) evaluate_tree(left);
+    if (center->terminal == 0) evaluate_tree(center);
+    if (right->terminal == 0) evaluate_tree(right);
+
+    //remove any children which contain the empty string (not relevant for evaluation)
+    if (strcmp(left->data, "e") == 0) {
+        free(left);
+        t->left = NULL;
+    }
+    if (strcmp(center->data, "e") == 0) {
+        free(center);
+        t->center = NULL;
+    }
+    if (strcmp(right->data, "e") == 0) {
+        free(right);
+        t->right = NULL;
+    }
+
+    //update the number of terminal and non-terminal children
+    num_terminal_children = 0;
+    num_non_terminal_children = 0;
+
+    if (left != NULL) {
+        if (left->terminal == 1) {
+            num_terminal_children++;
+        } else {
+            num_non_terminal_children++;
+        }
+    }
+    if (center != NULL) {
+        if (center->terminal == 1) {
+            num_terminal_children++;
+        } else {
+            num_non_terminal_children++;
+        }
+    }
+    if (right != NULL) {
+        if (right->terminal == 1) {
+            num_terminal_children++;
+        } else {
+            num_non_terminal_children++;
+        }
+    }
+
+    if (num_non_terminal_children > 0) {
+        //non terminal children exists only in the +x case
+        if (num_non_terminal_children == 1) {
+            //only 1 non terminal child, copy the information in its children to t's children
+            if (left != NULL) {
+                TREE tmp1 = left->left;
+                TREE tmp2 = left->center;
+                t->center = *(create_tree(tmp2->data));
+                left->data = tmp1->data;
+
+                left->terminal = 1;
+                center->terminal = 1;
+
+                free(tmp1);
+                free(tmp2);
+            } else if (center != NULL) {
+                TREE tmp1 = center->left;
+                TREE tmp2 = center->center;
+                t->left = *(create_tree(tmp1->data));
+                center->data = tmp2->data;
+
+                left->terminal = 1;
+                center->terminal = 1;
+
+                free(tmp1);
+                free(tmp2);
+            } else {
+                TREE tmp1 = right->left;
+                TREE tmp2 = right->center;
+                t->left = *(create_tree(tmp1->data));
+                t->center = *(create_tree(tmp2->data));
+
+                left->terminal = 1;
+                center->terminal = 1;
+
+                free(tmp1);
+                free(tmp2);
+                free(right);
+                t->right = NULL;
+            }
+        } else if (num_non_terminal_children == 2) {
+            //original experssion was of the form x+y+z, resulting in a +x+y tree structure (+x has "caught up" to +y)
+            TREE leftleft = left->left;
+            TREE leftcenter = left->center;
+            TREE centerleft = center->left;
+            TREE centercenter = center->center;
+
+            center->data = evaluate_node(leftcenter->data, centerleft->data, centercenter->data);
+            left->data = leftleft->data;
+
+            left->terminal = 1;
+            center->terminal = 1;
+
+            free(leftleft);
+            free(leftcenter);
+            free(centerleft);
+            free(centercenter);
+        } else {
+            //1 terminal child and 1 non-terminal child (the left operand has "caught up" to the operator and right operand)
+            TREE centerleft = center->left;
+            TREE centercenter = center->center;
+
+            t->data = evaluate_node(left->data, centerleft->data, centercenter->data);
+            t->terminal = 1;
+
+            free(centerleft);
+            free(centercenter);
+            free(left);
+            free(center);
+        }
+    } else {
+        //all children are terminals (recursion "succeeded")
+        if (num_terminal_children == 0) {
+            //leaf found (which shouldn't happen)
+            return "Unable to evaluate node with no children\n";
+        } else if (num_terminal_children == 1) {
+            //only child is 1 terminal, so bubble it up (will always be the left child because children are inserted left first)
+            t->data = left->data;
+            t->terminal = 1;
+            free(left);
+            t->left == NULL;
+        } else if (num_terminal_children == 2) {
+            //this is either the +x case (which is handled above), or it is the concatenation of a number and a digit
+            if (strcmp(t->data, "N") == 0) {
+                t->data = strcat(left->data, center->data);
+                t->terminal = 1;
+                free(left);
+                free(center);
+                t->left = NULL;
+                t->center = NULL;
+            }
+        } else {
+            //three terminal children, this means we have an expression to evaluate
+            t->data = evaluate_node(left->data, center->data, right->data);
+            t->terminal = 1;
+            free(left);
+            free(center);
+            free(right);
+            t->left = NULL;
+            t->center = NULL;
+            t->right = NULL;
+        }
     }
 
 }
